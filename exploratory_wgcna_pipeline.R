6973eebf22d3d8f860f821cc87db172efdefc308
Brandon Polzin <90783093+bjpolzin@users.noreply.github.com>
2023-04-11 18:29:56 -0500

    Update exploratory_wgcna_pipeline.R

--- exploratory_wgcna_pipeline.R
+++ exploratory_wgcna_pipeline.R
@@ -12,9 +12,9 @@ study_id <- ""  # Identifier for the study, tagged onto some output results
 
 # Set data directories
 expr_dir <-  ""  # Expression data directory
-# (Columns = sample IDs, rows = gene IDs)
-behav_dir <- ""   # Behavior data directory
-# (Columns = behaviors, rows = sample IDs)
+                 # (Columns = sample IDs, rows = gene IDs)
+behav_dir <- ""  # Behavior data directory
+                 # (Columns = behaviors, rows = sample IDs)
 
 # Set variables to filter out low data
 # (refer to ?cleanRNAseq::filter_low_genes() for details)
@@ -24,12 +24,12 @@ metric <- "units"
 
 # Indicate group selection based on sample ID
 qual <- ""  # Qualifier: string containing sample qualifier found in the
-# sample ID names (e.g. tissue type, brain region, etc.)
+            # sample ID names (e.g. tissue type, brain region, etc.)
 
 # Create vector of soft-threshold powers to investigate
 sft_powers <-  # Integer vector of soft-threshold powers to investigate
-# (6-12 is a good range, but computationally expensive;
-# review scale independence graph and change if necessary)
+               # (6-12 is a good range, but computationally expensive;
+               # review scale independence graph and change if necessary)
 
 # MODULE 1: Read in data and organize data for input to WGCNA
 source("./pipeline_modules/01_read_organize_data.R") 
